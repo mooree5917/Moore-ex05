88644be7f365713fc9fc95bf615ade4dc537232f
mooree5917 <90165352+mooree5917@users.noreply.github.com>
2021-09-06 03:28:48 -0400

    Add files via upload

---
+++ exercise05/src/main/java/Solution05/application.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Eric Moore
+ */
+package Solution05;
+
+import java.util.Scanner;
+
+public class application {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is the first number?: ");
+        int Int1 = in.nextInt();
+        System.out.print("What is the second number?: ");
+        int Int2 = in.nextInt();
+
+        System.out.printf(Int1 + " + " + Int2 + " = ", Int1 + Int2);
+        System.out.printf(Int1 + " - " + Int2 + " = ", Int1 - Int2);
+        System.out.printf(Int1 + " * " + Int2 + " = ", Int1 * Int2);
+        System.out.printf(Int1 + " / " + Int2 + " = ", Int1 / Int2);
+        }
+    }
